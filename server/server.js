9668bf9a39b32d3bad701c09495b767f928d789c
jungang0414 <dayr88520@gmail.com>
2024-12-25 18:35:44 +0800

    feat: Add server endpoints
    將uthRouter這個路由器，掛載到/api/auth這個路徑底下
    會根據請求的 URL 和 HTTP 方法來找到對應的處理函式，例如: http://localhost:4000/api/auth/login

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
-// 後端api
-app.get('/', (req, res) => res.send("API Working"))
+// API進入點
+app.get('/', (req, res) => res.send("API Working"));
+app.use('/api/auth', authRouter);
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
