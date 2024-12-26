7a83a1183ce5a8b80fb449bfd98725f5bf7f68f9
jungang0414 <dayr88520@gmail.com>
2024-12-26 16:55:20 +0800

    feat: User registration Send email notification
    用戶註冊成功透過Gmail傳送通知信件
    
    1. 使用 OAuth2 建立客戶端，並透過 Gmail API 取得的 Token 來進行 OAuth2 的驗證。
    2. 在取得存取權限後使用 nodemailer 來進行信件的傳輸，信件設定這裡接收傳入的 email、name 參數。
    3. 最後根據 nodemailer 傳輸的成功與否來判斷 Promise 是要兌現或拒絕。

---
+++ server/config/nodemailer.js
@@ -0,0 +1,70 @@
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
+
+const OAuth2 = google.auth.OAuth2;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+// Gmail API
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
+
+// 創建 OAuth2 client
+const handleSendMail = async (email, name) => {
+    return await new Promise((resolve, reject) => {
+
+        const oauth2Client = new OAuth2(
+            CLIENT_ID,
+            CLIENT_SECRET,
+            'https://developers.google.com/oauthplayground'
+        );
+
+        // OAuth2 refresh token
+        oauth2Client.setCredentials({
+            refresh_token: REFRESH_TOKEN
+        });
+
+        // 取得存取權
+        oauth2Client
+            .getAccessToken()
+            .then((res) => {
+                const accessToken = res.token;
+                if (!accessToken) {
+                    reject(new Error('取得 Access Token 失敗'));
+                }
+
+                // 寄送信件
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        type: 'OAuth2',
+                        user: process.env.SENDER_EMAIL,
+                        clientId: CLIENT_ID,
+                        clientSecret: CLIENT_SECRET,
+                        accessToken: accessToken ?? ''
+                    }
+                });
+
+                // 信件內容設定
+                const mailOptions = {
+                    form: process.env.SENDER_EMAIL,
+                    to: email,
+                    subject: "Welcome to MERN-AUTH",
+                    text: `Welcome, ${name}`
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        reject(error);
+                    }
+                    resolve({ message: 'Email寄送成功' });
+                });
+            })
+            .catch(() => {
+                reject(new Error('取得 Access Token 失敗'));
+            });
+    });
+}
+
+
+
+export default handleSendMail;
