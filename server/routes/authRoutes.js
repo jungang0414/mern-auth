785d724c425c4a184a31ccdd04ccc106f9614618
jungang0414 <dayr88520@gmail.com>
2024-12-27 16:24:18 +0800

    feat: use middleware
    在路由器中調用中介軟體
    
    因為我們在中介軟體中有使用 next() 這個方法，所以會接著繼續執行下一個中介軟體或者路由處理器
    這裡當使用者訪問/send-verify-otp 網址，會依序執行程式 userAuth > sendVerifyOtp

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,15 @@
 // 路由
 import express from 'express';
-import { login, logout, register } from '../controllers/authController.js';
+import { login, logout, register, sendVerifyOtp, verifyEamil } from '../controllers/authController.js';
+// 中介軟體
+import userAuth from '../middleware/userAuth.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEamil);
 
 export default authRouter;
