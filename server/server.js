8848a6d15d610b08fb0fccd1351ecb69225bc677
jungang0414 <dayr88520@gmail.com>
2025-03-18 17:33:34 +0800

    feat: add userRouter
    在伺服器中匯入使用者路由

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -18,5 +19,6 @@ app.use(cors({ credentials: true }));
 // API進入點
 app.get('/', (req, res) => res.send("API Working"));
 app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
