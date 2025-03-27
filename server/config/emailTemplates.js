e6ca944d5317909bda995805b6e40c624285457c
jungang0414 <dayr88520@gmail.com>
2025-03-27 18:39:12 +0800

    feat: updata config
    新增信件模板 emailTemplates.js
    於 nodemailer 匯入html模板用於信件中
    
    google OAuth2 有Token認證期效的問題之後可以新增服務帳戶來避免

---
+++ server/config/emailTemplates.js
@@ -0,0 +1,319 @@
+export const EMAIL_VERIFY_TEMPLATE = `
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>Email Verify</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Open Sans', sans-serif;
+      background: #E5E5E5;
+    }
+
+    table, td {
+      border-collapse: collapse;
+    }
+
+    .container {
+      width: 100%;
+      max-width: 500px;
+      margin: 70px 0px;
+      background-color: #ffffff;
+    }
+
+    .main-content {
+      padding: 48px 30px 40px;
+      color: #000000;
+    }
+
+    .button {
+      width: 100%;
+      background: #22D172;
+      text-decoration: none;
+      display: inline-block;
+      padding: 10px 0;
+      color: #fff;
+      font-size: 14px;
+      text-align: center;
+      font-weight: bold;
+      border-radius: 7px;
+    }
+
+    @media only screen and (max-width: 480px) {
+      .container {
+        width: 80% !important;
+      }
+
+      .button {
+        width: 50% !important;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+    <tbody>
+      <tr>
+        <td valign="top" align="center">
+          <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
+            <tbody>
+              <tr>
+                <td class="main-content">
+                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                      <tr>
+                        <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold;">
+                          驗證您的信箱
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
+                          只需一步即可驗證您的信箱: <span style="color: #4C83EE;">{{email}}</span>.
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700;">
+                          使用以下 OTP 來驗證您的帳戶
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 24px;">
+                          <p class="button" >{{otp}}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
+                          此 OTP 有效期限為 24 小時
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
+
+`
+
+export const PASSWORD_RESET_TEMPLATE = `
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>Password Reset</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Open Sans', sans-serif;
+      background: #E5E5E5;
+    }
+
+    table, td {
+      border-collapse: collapse;
+    }
+
+    .container {
+      width: 100%;
+      max-width: 500px;
+      margin: 70px 0px;
+      background-color: #ffffff;
+    }
+
+    .main-content {
+      padding: 48px 30px 40px;
+      color: #000000;
+    }
+
+    .button {
+      width: 100%;
+      background: #22D172;
+      text-decoration: none;
+      display: inline-block;
+      padding: 10px 0;
+      color: #fff;
+      font-size: 14px;
+      text-align: center;
+      font-weight: bold;
+      border-radius: 7px;
+    }
+
+    @media only screen and (max-width: 480px) {
+      .container {
+        width: 80% !important;
+      }
+
+      .button {
+        width: 50% !important;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+    <tbody>
+      <tr>
+        <td valign="top" align="center">
+          <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
+            <tbody>
+              <tr>
+                <td class="main-content">
+                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                      <tr>
+                        <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold;">
+                          忘記您的密碼?
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
+                          我們收到您帳戶的密碼變更請求: <span style="color: #4C83EE;">{{email}}</span>.
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700;">
+                          使用以下 OTP 來重設您的密碼
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 24px;">
+                          <p class="button" >{{otp}}</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 10px; font-size: 14px; line-height: 150%;">
+                          此密碼重設 OTP 有效期限為 15分鐘
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
+`
+
+export const REGISTER_TEMPLATE = `
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <title>Email Verify</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: 'Open Sans', sans-serif;
+      background: #E5E5E5;
+    }
+
+    table, td {
+      border-collapse: collapse;
+    }
+
+    .container {
+      width: 100%;
+      max-width: 500px;
+      margin: 70px 0px;
+      background-color: #ffffff;
+    }
+
+    .main-content {
+      padding: 48px 30px 40px;
+      color: #000000;
+    }
+
+    .button {
+      width: 100%;
+      background: #22D172;
+      text-decoration: none;
+      display: inline-block;
+      padding: 10px 0;
+      color: #fff;
+      font-size: 14px;
+      text-align: center;
+      font-weight: bold;
+      border-radius: 7px;
+    }
+
+    @media only screen and (max-width: 480px) {
+      .container {
+        width: 80% !important;
+      }
+
+      .button {
+        width: 50% !important;
+      }
+    }
+  </style>
+</head>
+
+<body>
+  <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" bgcolor="#F6FAFB">
+    <tbody>
+      <tr>
+        <td valign="top" align="center">
+          <table class="container" width="600" cellspacing="0" cellpadding="0" border="0">
+            <tbody>
+              <tr>
+                <td class="main-content">
+                  <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                      <tr>
+                        <td style="padding: 0 0 24px; font-size: 18px; line-height: 150%; font-weight: bold;">
+                          歡迎來到 MERN-AUTH
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700;">
+                          您註冊的大名為: <span style="color: #4C83EE;">{{name}}</span>.
+                        </td>
+                      </tr>
+                      <tr>
+                        <td style="padding: 0 0 16px; font-size: 14px; line-height: 150%; font-weight: 700;">
+                          信箱為: <span style="color: #4C83EE;">{{email}}</span>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
+`
--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -9,7 +9,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 
 // 創建 OAuth2 client
-const handleSendMail = async (email, name, subject, text) => {
+const handleSendMail = async (email, name, subject, html) => {
     return await new Promise((resolve, reject) => {
 
         const oauth2Client = new OAuth2(
@@ -49,7 +49,7 @@ const handleSendMail = async (email, name, subject, text) => {
                     form: process.env.SENDER_EMAIL,
                     to: email,
                     subject: subject,
-                    text: text
+                    html: html
                 };
 
                 transporter.sendMail(mailOptions, (error, info) => {
