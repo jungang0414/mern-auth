80446c274eb42daaa84eba8919825a5b2ee170fa
jungang0414 <dayr88520@gmail.com>
2025-03-27 17:24:43 +0800

    feat: updata origin
    cors 參數新增允許訪問的網址(這裡設定為本地前端的網址)

--- server/server.js
+++ server/server.js
@@ -11,10 +11,12 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
+// 允許訪問的網址列表
+const allowedOrigins = ['http://localhost:5173'] // ['', '']
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // API進入點
 app.get('/', (req, res) => res.send("API Working"));
